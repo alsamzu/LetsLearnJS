db306030207c2df78bb3968af4ca44f77f1016ac
alsamzu <zulfalsam@gmail.com>
2018-03-16 07:50:59 +0300

    binar search

--- custom.js
+++ custom.js
@@ -28,11 +28,28 @@
 //document.write(isFound);
 
 //EXAMPLLE 2 LINEAR SEARCH
+//function findIndex(values, target) {
+//  for(var i = 0; i < values.length; ++i){
+//    if (values[i] == target) { return target; }
+//  }
+//  return -1;
+//}
+//var ans=findIndex([7, 3, 6, 1, 0], 4);
+//alert(ans);
+//BINARY SEARCH EXAMPLE
 function findIndex(values, target) {
-  for(var i = 0; i < values.length; ++i){
-    if (values[i] == target) { return target; }
-  }
-  return -1;
+  return binarySearch(values, target, 0, values.length - 1);
+};
+
+function binarySearch(values, target, start, end) {
+  if (start > end) { return -1; } //does not exist
+
+  var middle = Math.floor((start + end) / 2);
+  var value = values[middle];
+
+  if (value > target) { return binarySearch(values, target, start, middle-1); }
+  if (value < target) { return binarySearch(values, target, middle+1, end); }
+  return middle; //found!
 }
-var ans=findIndex([7, 3, 6, 1, 0], 4);
+var ans=findIndex([1, 4, 6, 7, 12, 13, 15, 18, 19, 20, 22, 24], 20);
 alert(ans);
