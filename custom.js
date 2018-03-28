23fb216c8208c32aecb8fd5125d38c27c93a055c
alsamzu <zulfalsam@gmail.com>
2018-03-28 10:25:24 +0300

    date

--- custom.js
+++ custom.js
@@ -54,19 +54,24 @@
 //var ans=findIndex([1, 4, 6, 7, 12, 13, 15, 18, 19, 20, 22, 24], 20);
 //alert(ans);
 //FUNCTION TO CHECK IF A NUMBER IS PRIME OR NOT
-function prime(){
-    var a=prompt("Enter a number to check if it is a prime number");
-    var count = 0;
-    for( x=1;x<=a; x++){
-        if(a % x == 0){
-            count++;
-        }
-    }
-    if(count==2){
-        alert(a+" is a prime number");
-    }
-    else{
-        alert(a+" is not a prime  number"+"\n A prime number is a whole number greater than 1 whose only factors are 1 and itself e.g 2,11,37");
-    }
-}
-prime();
+//function prime(){
+//    var a=prompt("Enter a number to check if it is a prime number");
+//    var count = 0;
+//    for( x=1;x<=a; x++){
+//        if(a % x == 0){
+//            count++;
+//        }
+//    }
+//    if(count==2){
+//        alert(a+" is a prime number");
+//    }
+//    else{
+//        alert(a+" is not a prime  number"+"\n A prime number is a whole number greater than 1 whose only factors are 1 and itself e.g 2,11,37");
+//    }
+//}
+//prime();
+// FACTORS
+
+//ALERT DATE
+var currentDate = new Date();
+alert(currentDate);
