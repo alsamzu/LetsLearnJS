b2cb9f036627d2733da04b78c889a287160e8dd7
alsamzu <zulfalsam@gmail.com>
2018-03-28 13:11:53 +0300

    max number

--- custom.js
+++ custom.js
@@ -54,23 +54,23 @@
 //var ans=findIndex([1, 4, 6, 7, 12, 13, 15, 18, 19, 20, 22, 24], 20);
 //alert(ans);
 //FUNCTION TO CHECK IF A NUMBER IS PRIME OR NOT
-function prime(){
-    var a=prompt("Enter a number to check if it is a prime number");
-    var count = 0;
-    for( x=1;x<=a; x++){
-        if(a % x == 0){
-            count++;
-        }
-    }
-    if(count==2){
-        alert(a+" is a prime number");
-    }
-    else{
-        alert(a+" is not a prime  number"+"\n A prime number is a whole number greater than 1 whose only factors are 1 and itself e.g 2,11,37");
-    }
-}
-prime();
- FACTORS
+//function prime(){
+//    var a=prompt("Enter a number to check if it is a prime number");
+//    var count = 0;
+//    for( x=1;x<=a; x++){
+//        if(a % x == 0){
+//            count++;
+//        }
+//    }
+//    if(count==2){
+//        alert(a+" is a prime number");
+//    }
+//    else{
+//        alert(a+" is not a prime  number"+"\n A prime number is a whole number greater than 1 whose only factors are 1 and itself e.g 2,11,37");
+//    }
+//}
+//prime();
+// FACTORS
 
 //ALERT DATE
 //var currentDate = new Date();
@@ -80,3 +80,22 @@ prime();
 //document.getElementById("output").style.color="red";
 //TIP CALCULATOR
 
+//MAXIMUM NUMBER
+//function max(x,y){
+//    if(x>y){
+//        document.write(x+" is greater than "+y);
+//    }
+//    else{
+//        document.write(y+" is greater than "+x);
+//    }
+//}
+//max(15,5);
+
+function maX(){
+    var a= prompt("Enter the first number");
+     var b= prompt("Enter the second number");
+     var c= prompt("Enter the third number");
+  var d=  Math.max(a,b,c);
+    alert("You entered "+"\n"+a+"\n"+b+"\n"+c+"\n"+"The largest of the three numbers is "+d);
+}
+maX();
