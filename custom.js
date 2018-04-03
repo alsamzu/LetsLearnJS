1169dde5beadfa4a5fdd8b0aae951f112e633948
alsamzu <zulfalsam@gmail.com>
2018-04-03 08:46:01 +0300

    alerting current date

--- custom.js
+++ custom.js
@@ -1,23 +1,12 @@
 
 //Function for alerting current DATE
-//var currentDate = new Date();
-//window.alert(currentDate);
-//
-//document.getElementById("output").innerHTML="Hey there";
-//document.getElementById("output").style.color="red";
-//TIP CALCULATOR
+var currentDate = new Date();
+window.alert(currentDate);
+
+document.getElementById("output").innerHTML="Hey there";
+document.getElementById("output").style.color="red";
 
 
 
-//adding two numbers
-function addd(){
-    var a= prompt("Enter your first number");
-    var b = prompt("Enter your second number");
-    var c = prompt("Enter your third number");
-    var d = prompt("Enter your forth number");
-    
-    var e = parseInt(a)+parseInt(b)+parseInt(c)+parseInt(d);
-    document.write("\n"+e);
-}
 
-addd();
+
