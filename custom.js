9b910ed6681ef669a2cdb36b113200f2e92031b4
alsamzu <zulfalsam@gmail.com>
2018-04-03 08:38:56 +0300

    prime numbers

--- custom.js
+++ custom.js
@@ -2,23 +2,7 @@
 
 
 
-//FUNCTION TO CHECK IF A NUMBER IS PRIME OR NOT
-//function prime(){
-//    var a=prompt("Enter a number to check if it is a prime number");
-//    var count = 0;
-//    for( x=1;x<=a; x++){
-//        if(a % x == 0){
-//            count++;
-//        }
-//    }
-//    if(count==2){
-//        alert(a+" is a prime number");
-//    }
-//    else{
-//        alert(a+" is not a prime  number"+"\n A prime number is a whole number greater than 1 whose only factors are 1 and itself e.g 2,11,37");
-//    }
-//}
-//prime();
+
 // FACTORS
 
 //ALERT DATE
