7f16472896aa2ec36790cc951d2a698c51b6a14c
alsamzu <zulfalsam@gmail.com>
2018-04-03 08:36:07 +0300

    linear search algorithm

--- linearsearch.js
+++ linearsearch.js
@@ -11,3 +11,13 @@ function linear_search(arr,value){
 Array=[5,7,8,9,2,1];
 var isFound= linear_search(Array,8);
 document.write(isFound);
+
+//EXAMPLLE 2 LINEAR SEARCH
+function findIndex(values, target) {
+  for(var i = 0; i < values.length; ++i){
+    if (values[i] == target) { return target; }
+  }
+  return -1;
+}
+var ans=findIndex([7, 3, 6, 1, 0], 4);
+alert(ans);
