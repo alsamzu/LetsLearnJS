80c6dd6ff696a026f49a5a308c969ebda9f1eb09
alsamzu <zulfalsam@gmail.com>
2018-04-11 10:06:53 +0300

    switch statement

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 
 //Function for alerting current DATE
-var currentDate = new Date();
-window.alert(currentDate);
+//var currentDate = new Date();
+//window.alert(currentDate);
 
 //document.getElementById("output").innerHTML="Hey there";
 //document.getElementById("output").style.color="red";
@@ -12,28 +12,42 @@ window.alert(currentDate);
 //var 8=bye;
 
 
-Number
-FLOAT
-STRING " "
-Boolean true
-var e ="hello";
-var p ="5";
-parseInt(p);
-console.log(p);
-
-function max(){
- var a=prompt("enter your favorite drink");
-    if(a=="coffee"){
-        document.write("you like "+a);
-    }
-    else if(a=="tea"){
-        document.write("why do you like "+a);
-    }
-    else if(a=="soda"){
-        documet.write("hey "+a+" is unhealthy");
-    }
-    else{
-        document.write("what do you drink ");
-    }
+//Number
+//FLOAT
+//STRING " "
+//Boolean true
+//var e ="hello";
+//var p ="5";
+//parseInt(p);
+//console.log(p);
+//
+//function max(){
+// var a=prompt("enter your favorite drink");
+//    if(a=="coffee"){
+//        document.write("you like "+a);
+//    }
+//    else if(a=="tea"){
+//        document.write("why do you like "+a);
+//    }
+//    else if(a=="soda"){
+//        documet.write("hey "+a+" is unhealthy");
+//    }
+//    else{
+//        document.write("what do you drink ");
+//    }
+//}
+//max();
+var day="Tuesday";
+switch(day){
+    case "monday":
+        document.write("Yesss");
+        break;
+        case"Tuesday":
+        document.write("nope");
+        break;
+        case"Wednesday":
+        document.write("nopenope");
+        break;
+    default:
+        document.write("no date");
 }
-max();
