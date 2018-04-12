cbd8839fa5f355b6e2e81b4d31f1d4eb4549cf9c
alsamzu <zulfalsam@gmail.com>
2018-04-12 21:13:45 +0300

    validating an email

--- js/email.js
+++ js/email.js
@@ -1,12 +1,22 @@
 
 function mail(){
     var a = prompt("Enter an email address");
-    var b = a.includes("@");
-    if (b == true){
-        alert("This is an email address");
+//    THIS WORKS FINE
+//    var re = /\S+@\S+\.\S+/;
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var c = re.test(a);
+    
+    
+//     var b = a.includes("@" && ".");
+//    
+    if (c == true){
+        
+        alert("This is a valid email address");
     }
     else{
-        alert("This is not an email address");
-    }
+        alert("This is not a valid email address");
+    }  
+    
+   
 }
 mail();
