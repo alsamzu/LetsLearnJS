b062ebbd84d48084c4c3a7282c95c748cd0c603a
alsamzu <zulfalsam@gmail.com>
2018-04-20 07:01:26 +0300

    binary search

--- js/binarysearch.js
+++ js/binarysearch.js
@@ -15,3 +15,26 @@ function binarySearch(values, target, start, end) {
 }
 var ans=findIndex([1, 4, 6, 7, 12, 13, 15, 18, 19, 20, 22, 24], 20);
 alert(ans);
+//BINARY SEARCH EXAMPLE TWO
+var array=[55,77,62,80,32,13,68,10,23,52];
+function binarysearch(arr,value){
+    var high = arr.length-1;
+    var low = 0;
+    var mid = 0;
+    while(low <=high){
+        mid = Math.floor((high+low)/2);
+        if(arr[mid]==value){
+            return arr[mid];
+        }
+        else if(value>=arr[mid]){
+            //move the low up in one
+            low = mid+1;
+        }
+        else{
+            //move the high up one
+
+            high = mid -1;
+        }
+    }
+    return -1;
+}
