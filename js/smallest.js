5b0249ba1ba2e302c19ec4a7689e307b5d0c9c19
alsamzu <zulfalsam@gmail.com>
2018-04-22 16:56:39 +0300

    smllest int

--- js/smallest.js
+++ js/smallest.js
@@ -1,1 +1,33 @@
-Given an array of integers your solution should find the smallest integer.
+//Given an array of integers your solution should find the smallest integer.
+
+//  function findSmallestInt(args) {
+//    args=[23,54,1,34,6,78,6,2];
+//      smallest=0;
+//  for (i=0; i<args.length; i++){
+//       if (args[i] < smallest) {
+//        smallest = args[i];
+//      }
+//  }
+//  document.write(smallest);
+//  }
+//
+//findSmallestInt(args);
+
+function d(){
+    minarr=[];
+    for(i=0;i<3;i++){
+        var num=minarr.push(prompt("Enter a number"));
+
+    }
+     var c = Number.POSITIVE_INFINITY; ;
+    for(j=0;j<minarr.length;j++){
+
+        if(minarr[j]<c){
+            c = minarr[j];
+        }
+    }
+    alert("Your Entry is "+minarr+"\n"+" The Largest number is "+c);
+
+
+}
+d();
