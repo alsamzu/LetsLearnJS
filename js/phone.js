0557151d7235e971cc398dc7a79e191054077cd7
alsamzu <zulfalsam@gmail.com>
2018-04-23 19:55:45 +0300

    defined variables

--- js/phone.js
+++ js/phone.js
@@ -1,1 +1,8 @@
 //program to calculate price of your phone purchase
+const SPENDING_THRESHOLD = 200;
+const TAX_RATE=0.00;
+const PHONE_PRICE = 99.99;
+const ACCESORY_PRICE = 9.99;
+
+var bank_balance = 303.91;
+var amount = 0;
