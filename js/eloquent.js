50b7c7c9d181431545aab9a56cc278d7d424c60b
alsamzu <zulfalsam@gmail.com>
2018-04-24 18:09:05 +0300

    counting Bs

--- js/eloquent.js
+++ js/eloquent.js
@@ -111,4 +111,33 @@ function isEven(x){
         console.log(false);
     }
 }
-isEven(341);
+isEven(1);
+//Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase “B” characters there are in the string.
+//function countBs (x){
+//    var c = 0;
+//    for(i=0;i<x.length;i++){
+//        if(x[i]=="string"[B]){
+//            c++;
+//        }
+//    }
+//    document.write(c);
+//}
+//countBs("Because I Bought a Brunchy Buger");
+function countChar(string, ch) {
+  let counted = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == ch) {
+      counted += 1;
+    }
+  }
+  return counted;
+}
+
+function countBs(string) {
+  return countChar(string, "B");
+}
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
