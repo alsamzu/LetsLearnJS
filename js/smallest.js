200b55efd82bece6191501695bdd70370ae1ed6f
alsamzu <zulfalsam@gmail.com>
2018-04-24 17:12:56 +0300

    function to return minimum value

--- js/smallest.js
+++ js/smallest.js
@@ -13,21 +13,31 @@
 //
 //findSmallestInt(args);
 
-function d(){
-    minarr=[];
-    for(i=0;i<3;i++){
-        var num=minarr.push(prompt("Enter a number"));
-
+//function d(){
+//    minarr=[];
+//    for(i=0;i<3;i++){
+//        var num=minarr.push(prompt("Enter a number"));
+//
+//    }
+//     var c = Number.POSITIVE_INFINITY; ;
+//    for(j=0;j<minarr.length;j++){
+//
+//        if(minarr[j]<c){
+//            c = minarr[j];
+//        }
+//    }
+//    alert("Your Entry is "+minarr+"\n"+" The Largest number is "+c);
+//
+//
+//}
+//d();
+//Write a function min that takes two arguments and returns their minimum.
+function min(x,y){
+    if(x<y){
+        return x;
     }
-     var c = Number.POSITIVE_INFINITY; ;
-    for(j=0;j<minarr.length;j++){
-
-        if(minarr[j]<c){
-            c = minarr[j];
-        }
+    else{
+        return y;
     }
-    alert("Your Entry is "+minarr+"\n"+" The Largest number is "+c);
-
-
 }
-d();
+min(34,6;
