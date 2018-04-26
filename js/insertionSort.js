be1a742318c43ef4bd31c86bb0f0bb8e37093858
alsamzu <zulfalsam@gmail.com>
2018-04-26 08:21:16 +0300

    insertion sort

---
+++ js/insertionSort.js
@@ -0,0 +1,13 @@
+function insertionSort(array) {
+  var length = array.length;
+
+  for(var i = 1; i < length; i++) {
+    var temp = array[i];
+    for(var j = i - 1; j >= 0 && array[j] > temp; j--) {
+      array[j+1] = array[j];
+    }
+    array[j+1] = temp;
+  }
+
+  return array;
+}
