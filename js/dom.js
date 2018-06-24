3e85a8ebeed4e1200a42985b1e3daa3dc822e161
alsamzu <zulfalsam@gmail.com>
2018-06-24 21:16:27 +0300

    dom example

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,9 @@
-Finding HTML Elements by HTML Object Collections
+//Finding HTML Elements by HTML Object Collections
+
+var x = document.forms["frm1"];
+var text = "";
+var i;
+for (i = 0; i < x.length; i++) {
+    text += x.elements[i].value + "<br>";
+}
+document.getElementById("demo").innerHTML = text;
