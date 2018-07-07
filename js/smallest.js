4d5c293db2114e970c64241a33e1742c6aa8eb3e
alsamzu <zulfalsam@gmail.com>
2018-07-07 10:28:47 +0300

    finding smallest number in an array

--- js/smallest.js
+++ js/smallest.js
@@ -1,35 +1,40 @@
 //Given an array of integers your solution should find the smallest integer.
+//This function is not working because count if a fixed number
 
-  function findSmallestInt(min) {
+//  function findSmallestInt(min) {
+//var min=[];
+//      for(x=0;x<5;x++){
+//           var num = min.push(prompt("Enter a number"));
+//      }
+//
+//      smallest=min[0];
+//  for (i=0; i<min.length; i++){
+//       if (min[i] < smallest) {
+//        smallest = min[i];
+//      }
+//  }
+//  document.write(smallest);
+//  }
+//
+//findSmallestInt();
 
-      smallest=min[0];
-  for (i=0; i<min.length; i++){
-       if (min[i] < smallest) {
-        smallest = min[i];
-      }
-  }
-  document.write(smallest);
-  }
+function d(){
+    minarr=[];
+    for(i=0;i<3;i++){
+        var num=minarr.push(prompt("Enter a number"));
 
-findSmallestInt([23,54,15,34,6,78,6,27]);
+    }
+     var c = Number.POSITIVE_INFINITY; ;
+    for(j=0;j<minarr.length;j++){
 
-//function d(){
-//    minarr=[];
-//    for(i=0;i<3;i++){
-//        var num=minarr.push(prompt("Enter a number"));
-//
-//    }
-//     var c = Number.POSITIVE_INFINITY; ;
-//    for(j=0;j<minarr.length;j++){
-//
-//        if(minarr[j]<c){
-//            c = minarr[j];
-//        }
-//    }
-//    alert("Your Entry is "+minarr+"\n"+" The Largest number is "+c);
-//
-//
-//}
-//d();
+        if(minarr[j]<c){
+            c = minarr[j];
+        }
+    }
+    alert("Your Entry is "+minarr+"\n"+" The Smallest number is "+c);
+
+
+}
+d();
 
 
