498d87778cc0dc65242b10adac89583972f19018
alsamzu <zulfalsam@gmail.com>
2018-07-26 21:07:28 +0300

    js Example

--- js/adding.js
+++ js/adding.js
@@ -1,27 +1,27 @@
 //FUNCTION TO ADD TWO NUMBERS WITHOUT USER INPUT
-function add(x,y){
-    var z = document.write(x+y);
-    
+function add(x, y) {
+    var z = document.write(x + y);
+
 }
-add(4,5);
+add(4, 5);
 //adding two numbers with user input
-function addd(){
-    var a= prompt("Enter your first number");
+function addd() {
+    var a = prompt("Enter your first number");
     var b = prompt("Enter your second number");
-    var c = parseInt(a)+parseInt(b);
-    document.write("\n"+c);
+    var c = parseInt(a) + parseInt(b);
+    document.write("\n" + c);
 }
 
 addd();
 //Adding more than two numbers
-function addd(){
-    var a= prompt("Enter your first number");
+function addd() {
+    var a = prompt("Enter your first number");
     var b = prompt("Enter your second number");
     var c = prompt("Enter your third number");
     var d = prompt("Enter your forth number");
-    
-    var e = parseInt(a)+parseInt(b)+parseInt(c)+parseInt(d);
-    document.write("\n"+e);
+
+    var e = parseInt(a) + parseInt(b) + parseInt(c) + parseInt(d);
+    document.write("\n" + e);
 }
 
 addd();
