f4de8adaf9761ba76672cf266d4d64b152bfe7cb
alsamzu <zulfalsam@gmail.com>
2018-08-18 22:08:02 +0300

    email.js

--- js/email.js
+++ js/email.js
@@ -1,22 +1,20 @@
-
-function mail(){
+function mail() {
     var a = prompt("Enter an email address");
-//    THIS WORKS FINE
-//    var re = /\S+@\S+\.\S+/;
+    //    THIS WORKS FINE
+    //    var re = /\S+@\S+\.\S+/;
     var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     var c = re.test(a);
-    
-    
-//     var b = a.includes("@" && ".");
-//    
-    if (c == true){
-        
+
+
+    //     var b = a.includes("@" && ".");
+    //
+    if (c == true) {
+
         alert("This is a valid email address");
-    }
-    else{
+    } else {
         alert("This is not a valid email address");
-    }  
-    
-   
+    }
+
+
 }
 mail();
