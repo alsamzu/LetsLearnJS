1b74e8a9e821feef5928960798c50402ec129873
alsamzu <zulfalsam@gmail.com>
2018-08-23 13:15:09 +0300

    hash tables js

--- js/hashTables.js
+++ js/hashTables.js
@@ -1,18 +1,18 @@
 function HashTable(size) {
-  this.size = size;
-  this.buckets = new Array(size);
+    this.size = size;
+    this.buckets = new Array(size);
 
-  this.add = function(value) {
-    var index = this.hash(value);
-    this.buckets[index] = value;
-  };
-  this.hash = function(value) {
-    var sum = 0;
-    for (var i = 0; i < value.length; ++i) {
-      sum += value[i].charCodeAt() - 97;
-    }
-    return sum % this.size;
-  };
+    this.add = function (value) {
+        var index = this.hash(value);
+        this.buckets[index] = value;
+    };
+    this.hash = function (value) {
+        var sum = 0;
+        for (var i = 0; i < value.length; ++i) {
+            sum += value[i].charCodeAt() - 97;
+        }
+        return sum % this.size;
+    };
 }
 
 var hash = new HashTable(3);
