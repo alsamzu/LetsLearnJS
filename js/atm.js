99da606ef01ad60eda84ce3d80d8ca7cb46d5bcb
alsamzu <zulfalsam@gmail.com>
2018-11-18 21:27:31 +0300

    atm program js

---
+++ js/atm.js
@@ -0,0 +1,30 @@
+ pin=[1234,5678,9000];
+
+
+
+
+function atm(){
+
+ for(a=2; a>=0; a--){
+     var pinchk= prompt("Enter your pin:");
+
+        var pint = parseInt(pinchk) ;
+     if(pint==pin[0]|pint==pin[1]|pint==pin[2]){
+         alert("Welcome to the system.\n Pleae select an option\n 1.Check Balance \n 2.Deposit Cash \n 3.Withdraw Cash");
+          break;
+     }
+
+     else{
+         alert("Incorrect pin.Please try again. You have "+a+" attempts left");
+     }
+
+
+    }
+
+
+
+
+
+
+}
+atm();
