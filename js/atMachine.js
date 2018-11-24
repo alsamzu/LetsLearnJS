8a7bc832d51b63aed5d1d9348aa346020e5c1925
alsamzu <zulfalsam@gmail.com>
2018-11-24 21:28:48 +0300

    Atm js

--- js/atMachine.js
+++ js/atMachine.js
@@ -2,7 +2,7 @@ function atm(){
 for(i=2; i>=0; i--){
 var pin=prompt("Enter your pin");
 if (pin==1234){
-alert("Welcome to our system\n 1.Check Balance\n 2.Withdrawal\n 3.Deposit\n");
+alert("Welcome to our ATM system\n 1.Check Balance\n 2.Withdrawal\n 3.Deposit\n");
     balance==20000;
    var proceed=prompt("Enter the number");
    if(proceed==1){
