4b99092281b28d8cb574b0cc90c17d44382c2d20
alsamzu <zulfalsam@gmail.com>
2019-02-20 10:10:44 +0300

    js

--- js/check6.js
+++ js/check6.js
@@ -1,29 +1,28 @@
-function d(){
-checkarr=[];
-    for(i=0;i<3;i++){
-        var num=checkarr.push(prompt("Enter a number"));
+function d() {
+    checkarr = [];
+    for (i = 0; i < 3; i++) {
+        var num = checkarr.push(prompt("Enter a number"));
 
     }
-     var check =0;
-    for(j=0;j<checkarr.length;j++){
+    var check = 0;
+    for (j = 0; j < checkarr.length; j++) {
 
-        if(checkarr[j]>6){
-           check++;
+        if (checkarr[j] > 6) {
+            check++;
         }
 
     }
-    if(check>=1){
+    if (check >= 1) {
         alert("Found a number greater than 6");
-    }
-
-    else{
+    } else {
         alert("All entries are less than 6");
     }
 
 
 
-//    alert("Your Entry is "+checkarr+"\n"+" The Largest number is "+c);
+    //    alert("Your Entry is "+checkarr+"\n"+" The Largest number is "+c);
 
 
-}56
+}
+56
 d();
